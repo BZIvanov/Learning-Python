cf13a9b07b7d5b3c416f7525b5aa647547d2fcd5
BZIvanov <biser.z.ivanov@gmail.com>
2024-02-02 20:57:24 +0200

    nonlocal example

--- scope.py
+++ scope.py
@@ -12,7 +12,7 @@ print(get_num())
 count = 0
 
 
-# with the keyword global, the increment function will search for count variable in its scope and will throw an error
+# without the keyword global, the increment function will search for count variable in its scope and will throw an error for not finding it
 def increment():
     global count
     count += 1
@@ -20,3 +20,19 @@ def increment():
 
 
 print(increment())
+
+
+# nonlocal is used within a nested function to indicate that a variable refers to a variable in the nearest enclosing scope that is not global.
+# it allows you to modify a variable in an outer (but non-global) scope.
+def outer_function():
+    x = 10
+
+    def inner_function():
+        nonlocal x
+        x = 20
+        print(x) # 20
+
+    inner_function()
+    print(x) # 20
+
+outer_function()
