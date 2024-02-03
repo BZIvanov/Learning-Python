69c2a82071e6d5f9f455726a686809862946d7f8
BZIvanov <ivanov.biser@gmx.com>
2024-02-03 19:22:23 +0200

    import example

--- lambdas.py
+++ lambdas.py
@@ -1,2 +1,9 @@
 sum_nums = lambda a, b: a + b
+
 print(sum_nums(2, 3))  # 5
+
+nums_list = [1, 3, 5, 4, 9]
+
+squared_nums = map(lambda num : num * num, nums_list)
+
+print(list(squared_nums)) # [1, 9, 25, 16, 81]
